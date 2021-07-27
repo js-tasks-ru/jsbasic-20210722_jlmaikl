f10d47e0052a3f5223b622749eb85eaa7bd537c7
Mihail Lomako <jlmaikl@mail.ru>
2021-07-27 13:27:39 +0300

    решение 1 задачи 1-го модуля

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,17 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1;
+  if (n == 0 || n == 1) {
+    return 1;
+  } else {
+  while (n > 1) {
+    result *= n--;
+  }  
+  return result;
 }
+}
+
+
+
+
+
+
